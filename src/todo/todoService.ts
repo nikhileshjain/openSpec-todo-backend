fce21a80312eeb86fec8dcaedd4f44c0610a1844
nikhileshjain <nikhilesh.jain07@gmail.com>
2024-01-16 19:50:50 +0530

    Basic BE Done

---
+++ src/todo/todoService.ts
@@ -0,0 +1,25 @@
+import { Todo } from "./todo";
+
+export type TodoCreationParams = Pick<Todo,"title" | "description">
+
+export class TodoService{
+    static get: any;
+    public get (todoId:string): Todo {
+        return {
+            id: todoId,
+            description: "hello",
+            title:"yellow",
+            done: false
+        }
+    }
+
+    public create (todoCreationParams: TodoCreationParams): Todo {
+        console.log("mock db call")
+        return {
+            id: "123",
+            description: "hello",
+            title:"yellow",
+            done: false
+        }
+    }
+}
